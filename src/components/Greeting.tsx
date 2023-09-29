64cdf8d4c2e0fbd3aa4d9c422e35dad04c1956b0
Aekkrit Kuntipalo <aekkritkuntipalo@Aekkrits-MacBook-Air.local>
2023-09-29 14:47:39 +0700

    Greeting-components

---
+++ src/components/Greeting.tsx
@@ -0,0 +1,11 @@
+import classes from './Greeting.module.css'
+
+const Greeting = () => {
+  return (
+    <div className={classes.card}>
+      <h3>Welcome!</h3>
+      <p>PomPam</p>
+    </div>
+  )
+}
+export default Greeting
