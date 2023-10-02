49cee337a34a0bdb9feca7b490e885ecebb317f6
Aekkrit Kuntipalo <aekkritkuntipalo@Aekkrits-MacBook-Air.local>
2023-10-02 11:08:47 +0700

    demo props

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
 import Greeting from './components/Greeting'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Greeting />
+      <Greeting name="PomPam" greetingMsg="Hello!" />
     </div>
   )
 }
