cf0d0b8dc4268d42bf23ca1a6e3f4761bd5caa46
Aekkrit Kuntipalo <aekkritkuntipalo@Aekkrits-MacBook-Air.local>
2023-10-04 16:49:14 +0700

    demo/useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,84 @@
 import './App.css'
 import Greeting from './components/Greeting'
 import Navbar from './components/Navbar'
-import { PostDTO } from './types/dto'
+import { CreatePostDTO, PostDTO } from './types/dto'
 import Post from './components/Post'
-import { FormEvent, useState } from 'react'
-
-const initialPosts: PostDTO[] = [
-  {
-    id: 1,
-    userId: 1,
-    title: "Let's learn React!",
-    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-  },
-  {
-    id: 2,
-    userId: 2,
-    title: 'How to install Node.js',
-    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-  },
-  {
-    id: 3,
-    userId: 3,
-    title: 'Basic HTML',
-    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-  },
-]
+import { FormEvent, useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [posts, setPosts] = useState<PostDTO[]>(initialPosts)
+  const [posts, setPosts] = useState<PostDTO[] | null>(null)
   const [newTitle, setNewTitle] = useState<string>('')
   const [newBody, setNewBody] = useState<string>('')
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [isPending, setIsPending] = useState<boolean>(false)
 
-  const handleSubmit = (e: FormEvent) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true)
+      try {
+        // const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+        // const data = await res.json()
+
+        // if (!res.ok) {
+        //   throw new Error('error')
+        // }
+
+        const res = await axios.get<PostDTO[]>('https://jsonplaceholder.typicode.com/posts')
+
+        setPosts(res.data)
+      } catch (err) {
+        console.error(err)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault()
+    setIsPending(true)
 
-    const currentPosts = [...posts]
+    // if (!posts) return
 
-    currentPosts.push({
-      id: Math.floor(Math.random() * 1000), // * database should generate id for us
-      userId: Math.floor(Math.random() * 1000),
+    const data: CreatePostDTO = {
       title: newTitle,
       body: newBody,
-    })
+      userId: Math.floor(Math.random() * 1000),
+    }
 
-    setPosts(currentPosts)
+    try {
+      const response = await axios.post<PostDTO>('https://jsonplaceholder.typicode.com/posts', data, {
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+
+      console.log(response.data)
+    } catch (err) {
+      console.error(err)
+    } finally {
+      setIsPending(false)
+    }
+
+    // const currentPosts = [...posts]
+
+    // currentPosts.push({
+    //   id: Math.floor(Math.random() * 1000), // * database should generate id for us
+    //   userId: Math.floor(Math.random() * 1000),
+    //   title: newTitle,
+    //   body: newBody,
+    // })
+
+    // setPosts(currentPosts)
 
     // Clear form after set posts
     setNewBody('')
     setNewTitle('')
   }
-
+  if (isLoading) return <h1>Loading...</h1>
   return (
     <div className="App">
       <Navbar />
@@ -60,14 +89,14 @@ function App() {
         <input type="text" value={newTitle} onChange={(e) => setNewTitle(e.target.value)} required />
         <label>Body</label>
         <input type="text" value={newBody} onChange={(e) => setNewBody(e.target.value)} required />
-
-        <button type="submit">Submit</button>
+        {isPending ? <button disabled>Pending</button> : <button type="submit">Submit</button>}
       </form>
 
       <div className="feed-container">
-        {posts.map((post) => {
-          return <Post key={post.id} post={post} />
-        })}
+        {posts &&
+          posts.map((post) => {
+            return <Post key={post.id} post={post} />
+          })}
       </div>
     </div>
   )
